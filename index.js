6ffa5f2f4bc3d79721db6bd64a12eb47d703cb1e
Brandon Hamilton <bham2015@gmail.com>
2024-09-18 22:07:06 -0500

    The changes in this commit will set up the state for the web page.

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/event
 
 // === State ===
 
+const state = {
+    events: [],
+};
+
+async function getEvents() {
+    try {
+        const response = await fetch(API_URL);
+        const responseObj = await response.json();
+        state.events = responseObj.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 // === Render ===
 
+
+async function render() {
+    await getEvents();
+}
+
 // === Script ===
+
+render();
